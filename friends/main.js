4f674e26ea37b7681cd20690b6edcf9e4056bf7e
Daniel008-09 <158571526+Daniel008-09@users.noreply.github.com>
2024-02-20 14:18:10 +0600

    Add files via upload
    sdfghj

---
+++ friends/main.js
@@ -0,0 +1,79 @@
+// const card = [
+//     {
+//         name: Doni,
+//         age: 15,
+//         friends: {
+//             Eldar:{
+//                 age: 15,
+//                 school: number14,
+//                 hobbi:{
+//                     first: football,
+//                     second: tennis,
+//                 },
+//                 adrres: {
+
+//                 }
+//             },
+//             Bayaman: {
+
+//             }
+//         },
+//         hobby: {
+//             first: study,
+//             second: draw,
+//         },
+//         family: {
+//             mama: {
+//                 name: Ainagul,
+//                 age: 39,
+//             },
+//             dad: {
+//                 colligy: {
+
+//                 }
+//             }
+//         }
+//     }
+// ]
+
+
+const card = [
+    {
+        name:Daniel,
+        last_name:zoldoshev,
+        age: 15,
+        gender: male,
+        maried: false,
+        hobby{
+            first:football,
+            second:armwrestling,
+            third:run
+        },
+        friends{
+            bayaman{
+                age:14,
+                gender:male,
+                maried:false,
+                address{
+                    street:kulatov,
+                    number:47,
+                },
+                hobby{
+                    first:wrestling, 
+                },
+            },
+
+            
+        }
+        family{
+            mother:Ainagul,
+            last_name:Kachybekova,
+            age:39,
+
+
+
+        }
+
+
+    }
+]
---
+++ friends/script.js
@@ -0,0 +1,443 @@
+// const obj = {
+//     name: 'Daniel',
+//     last_name: 'joldoshev',
+//     phone: '+996 998 60 09 08',
+//     meried: false,
+//     gender: 'male',
+//     address: {
+//         street: 'tashiev',
+//         number: 32,
+//         country: 'KG',
+//         region: 'osh',
+//         city: 'osh'
+//     },
+//     frends: [
+//         {
+//             name: 'Баяман',
+//             last_name: 'махаматисаев',
+//             phone: '+996 554 844 458',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'усаналиев',
+//                 number: 53,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh'
+//             },
+//             place of study: {
+//                 address: {
+//                     street: 'Kulatov',
+//                     number: 14,
+//                     country: 'KG',
+//                     region: 'osh',
+//                     city: 'osh'
+//             },
+//         },
+            
+//                 name:'kursant',
+//                 last_name: 'Abdubaliev',
+//                 phone: '+996 703 287 528',
+//                 meried: 'false',
+//                 gender: 'male',
+//                 address: {
+//                     street: 'ishkavan',
+//                     number: '34',
+//                     country: 'KG',
+//                     region: 'osh',
+//                     city: 'osh',
+//                 },
+
+//                 place of study: {
+//                     address: {
+//                         street: 'Kulatov',
+//                         number: 14,
+//                         country: 'KG',
+//                         region: 'osh',
+//                         city: 'osh'
+//                 },
+                
+
+//             },
+//         {
+//             name: 'timur',
+//             last_name: 'mashirapov',
+//             phone: '+996 550 770 383',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'кирпичный',
+//                 number: 31,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh'
+//             },
+//             place of study: {
+//                 address: {
+//                     street: 'Kulatov',
+//                     number: 14,
+//                     country: 'KG',
+//                     region: 'osh',
+//                     city: 'osh'
+//             },
+//         },
+//         k:{
+//             name: 'Aman',
+//             last_name: 'imanaliev',
+//             phone: '+996 732 21 84 66',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'smmutu',
+//                 number: 26,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh',
+//             },
+            
+
+            
+//         },
+//         {
+//             name: 'Arsen',
+//             last_name: 'Erkinbekov',
+//             phone: '+996 732 21 54 98',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'Anar',
+//                 number: 19,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh',
+//             },  
+//         }
+//         ,{
+//             name: 'Alesha',
+//             last_name: 'Agiev',
+//             phone: '+996 732 01 62 11',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'smmutu',
+//                 number: 26,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh',
+//             },
+            
+
+            
+//         },
+//         {
+//             name: 'Aman',
+//             last_name: 'imanaliev',
+//             phone: '+996 732 21 84 66',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'smmutu',
+//                 number: 26,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh',
+//             },
+            
+
+            
+//         }
+
+//         ,{
+//             name: 'janbolot',
+//             last_name: 'sultanbekov',
+//             phone: '+996 732 54 62 18',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'usonaliev',
+//                 number: 36,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh',
+//             },
+            
+
+            
+//         },
+//     ],
+
+//     famaly: [
+//         {
+//             skills: [
+//                  'js', 
+//             ],
+
+//             name: 'Sauz',
+//             role: 'father',
+//             last_name: 'Zoldoshev',
+//             phone: '+996 732 21 84 69',
+//             meried: true,
+//             gender: 'male',
+//             address: {
+//                 street: 'tashiev',
+//                 number: 32,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh'
+//             },
+//         },
+//         {
+//             name: 'Ainagul',
+//             role: 'mother',
+//             last_name: 'Kachybekova',
+//             phone: '+996 732 21 55 29',
+//             meried: true,
+//             gender: 'woman',
+//             address: {
+//                 street: 'tashiev',
+//                 number: 32,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh'
+//             },
+//         },
+//         {
+//             name: 'Nazima',
+//             last_name: 'Kachybekova',
+//             role: 'sister',
+//             phone: 'none',
+//             meried: false,
+//             gender: 'woman',
+//             address: {
+//                 street: 'Tashiev',
+//                 number: 32,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh'
+//             },
+//         }
+
+//         ,{
+//             name: 'Davilet',
+//             last_name: 'Zoldoshev',
+//             role: 'brother',
+//             phone: 'none',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'Tashiev',
+//                 number: 32,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh'
+//             },
+//         }
+
+//         ,{
+//             name: 'Alihan',
+//             last_name: 'Zoldoshev',
+//             role: 'brother',
+//             phone: 'none',
+//             meried: false,
+//             gender: 'male',
+//             address: {
+//                 street: 'Tashiev',
+//                 number: 32,
+//                 country: 'KG',
+//                 region: 'osh',
+//                 city: 'osh'
+//             },
+//         }
+
+
+//     ]
+//     ,
+//     skills: [
+//          'js',
+//     ]
+
+// }
+
+
+// console.log(obj.frends[2].name);
+
+const obj = 
+[{
+    name: 'Daniel',
+    last_name: 'zoldoshev',
+    phone: '+996 772 21 84 66',
+    meried: false,
+    gender: 'male',
+    address: {
+        street: 'tashiev',
+        number: 32,
+        country: 'KG',
+        region: 'osh',
+        city: 'osh'
+    },
+    frends: 
+        {
+            name: 'bayaman',
+            last_name: 'mahamatisaev',
+            phone: '+996 732 21 84 66',
+            meried: false,
+            gender: 'male',
+            address: {
+                street: 'ysonaliev',
+                number: 25,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        
+         {   name: 'timur',
+            last_name: 'mashirapov',
+            phone: '+996 732 21 84 66',
+            meried: false,
+            gender: 'male',
+            address: {
+                street: 'manakulova',
+                number: 51,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'kursant',
+            last_name: 'abdubaliev',
+            phone: '+996 732 21 84 66',
+            meried: false,
+            gender: 'male',
+            address: {
+                street: 'ishkavan',
+                number: 42,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        }
+        
+             {   name: 'aman',
+                last_name: 'imanaliev',
+                phone: '+996 732 21 84 66',
+                meried: false,
+                gender: 'male',
+                address: {
+                    street: 'smuttu',
+                    number: 29,
+                    country: 'KG',
+                    region: 'osh',
+                    city: 'osh'
+                },
+            }
+
+                {
+                    name: 'janbolot',
+                    last_name: 'sultanbekov',
+                    phone: '+996 732 21 84 66',
+                    meried: false,
+                    gender: 'male',
+                    address: {
+                        street: 'usonaliev',
+                        number: 32,
+                        country: 'KG',
+                        region: 'osh',
+                        city: 'osh'
+                    },
+                }
+
+
+                    
+                    {    name: 'alesha',
+                        last_name: 'agiev',
+                        phone: '+996 732 21 84 66',
+                        meried: false,
+                        gender: 'male',
+                        address: {
+                            street: 'kulatov',
+                            number: 53,
+                            country: 'KG',
+                            region: 'osh',
+                            city: 'osh'
+                        },
+                    }
+
+
+                        {
+                            name: 'arsen',
+                            last_name: 'erkinbekov',
+                            phone: '+996 732 21 84 66',
+                            meried: false,
+                            gender: 'male',
+                            address: {
+                                street: 'anar',
+                                number: 28,
+                                country: 'KG',
+                                region: 'osh',
+                                city: 'osh'
+                            },
+                        }
+
+   { famaly: 
+        
+            skills: [
+                 'js',
+            ],
+
+        {    name: 'Sauz',
+            role: 'father',
+            last_name: 'zoldoshev',
+            phone: '+996 732 21 84 66',
+            meried: true,
+            gender: 'male',
+            address: {
+                street: 'tashiev',
+                number: 32,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh'
+            },
+        },
+        {
+            name: 'Ainagul',
+            role: 'mother',
+            last_name: 'Kachybekova',
+            phone: '+996 732 21 84 66',
+            meried: true,
+            gender: 'woman',
+            address: {
+                street: 'tashiev',
+                number: 32,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh',
+            },
+        },
+        {
+            name: 'Nazima',
+            last_name: 'Kachybekova',
+            role: 'sister',
+            phone: 'none',
+            meried: false,
+            gender: 'false',
+            address: {
+                street: 'tashiev',
+                number: 32,
+                country: 'KG',
+                region: 'osh',
+                city: 'osh',
+            },
+        }
+        },
+    
+    skills: [
+         'js', 
+    ]
+
+
+]
+
+
+console.log(obj.frends[2].phone);
